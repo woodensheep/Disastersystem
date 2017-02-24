7b6a938b496c25b26c25cf34d263509eae630187
lemon <limenglly@163.com>
2017-02-24 17:10:39 +0800

    20170224下午

--- app/src/main/java/com/nandity/disastersystem/activity/MainActivity.java
+++ app/src/main/java/com/nandity/disastersystem/activity/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void setListeners() {
         mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
