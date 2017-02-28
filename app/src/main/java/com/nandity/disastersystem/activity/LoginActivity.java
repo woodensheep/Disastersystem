09996f6d1bc67f2261bfe509cb5cb6d43cda8db3
lemon <limenglly@163.com>
2017-02-28 15:57:41 +0800

    20170228

--- app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
+++ app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
@@ -54,6 +54,7 @@ public class LoginActivity extends AppCompatActivity{
                         public void run() {
                             Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                             startActivity(intent);
+                            finish();
                         }
                     },3000);
                 }
