598add30900ae14b1b170c0e7f47d229199edbe4
lemon <limenglly@163.com>
2017-02-28 15:35:26 +0800

    20170228

--- app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
+++ app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.nandity.disastersystem.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -47,6 +49,13 @@ public class LoginActivity extends AppCompatActivity{
                 if (mBtnLogin.isProgressStop()){
                     //跳转主页面
                     //TODO
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                        }
+                    },3000);
                 }
             }
         });
