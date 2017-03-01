a3d53053369523d2438f10aeaac2ecf243079bad
xingyufeifan <634846539@qq.com>
2017-03-01 14:59:18 +0800

    编写登录功能

--- app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
+++ app/src/main/java/com/nandity/disastersystem/activity/LoginActivity.java
@@ -1,101 +1,188 @@
 package com.nandity.disastersystem.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nandity.disastersystem.R;
-import com.nandity.disastersystem.view.AnimationButton;
+import com.nandity.disastersystem.constant.ConnectUrl;
+import com.nandity.disastersystem.utils.ToastUtils;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * 登录界面
  * Created by baohongyan on 2017/2/23.
  */
-public class LoginActivity extends AppCompatActivity{
-
-    private EditText et_name;
-    private EditText et_pwd;
-    private AnimationButton mBtnLogin;
-    private TextView tv_forgetpwd;
-    private TextView tv_settings;
-
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    @BindView(R.id.et_name)
+    EditText etName;
+    @BindView(R.id.et_pwd)
+    EditText etPwd;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.settings)
+    TextView settings;
+    private String name, pwd;
+    private String sessiongId;
+    private SharedPreferences sp;
+    private ProgressDialog progressDialog;
+    private boolean isLogin=false;
+    private Context context;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+        context=this;
         initView();
+        setListener();
     }
 
-    private void initView(){
-        et_name = (EditText)findViewById(R.id.et_name);
-        et_pwd = (EditText)findViewById(R.id.et_pwd);
-        mBtnLogin = (AnimationButton)findViewById(R.id.btn_login);
-        tv_forgetpwd = (TextView)findViewById(R.id.forgetpwd);
-        tv_settings = (TextView)findViewById(R.id.settings);
+    private void initView() {
+        etName.setText(sp.getString("userName",""));
+        etPwd.setText(sp.getString("passWord",""));
+        progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setMessage("正在登录...");
+    }
 
-        mBtnLogin.setText("登录");
-        mBtnLogin.setMode(AnimationButton.Mode.Hand_Finish);
-        mBtnLogin.setOnAnimationButtonClickListener(new AnimationButton.OnAnimationButtonClickListener() {
+    private void setListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick() {
-                mBtnLogin.stopProgress(); //stopProgress方法仅在button.setMode(AnimationButton.Mode.Hand_Finish)之后才有效。
-                if (mBtnLogin.isProgressStop()){
-                    //跳转主页面
-                    //TODO
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    },3000);
+            public void onClick(View v) {
+                if (validate()) {
+                    btnLogin.setEnabled(false);
+                    progressDialog.show();
+                    login();
                 }
             }
         });
+        settings.setOnClickListener(new View.OnClickListener() {
 
-        tv_settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                View view= LayoutInflater.from(context).inflate(R.layout.dialog_setting,null);
+                final EditText etIp= (EditText) view.findViewById(R.id.setting_ip);
+                final EditText etPort= (EditText)view.findViewById(R.id.setting_port);
+                etIp.setText(sp.getString("IP",""));
+                etPort.setText(sp.getString("PORT",""));
+                new AlertDialog.Builder(context)
+                        .setView(view)
+                        .setTitle("设置")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String ip=etIp.getText().toString().trim();
+                                String port=etPort.getText().toString().trim();
+                                sp.edit().putString("IP",ip).apply();
+                                sp.edit().putString("PORT",port).apply();
+                                ToastUtils.showShortToast("设置成功");
+                                dialog.dismiss();
+                            }
+                        }).show();
+
+
+
             }
         });
     }
 
-    //登录
-    public void login(){
-        if (!validate()){
-            mBtnLogin.setEnabled(true);
-            return;
-        }
 
+    private void login() {
+        sessiongId = UUID.randomUUID().toString();
+        sp.edit().putString("sessionId", sessiongId).apply();
+        Log.d(TAG, "账号：" + name);
+        Log.d(TAG, "密码：" + pwd);
+        Log.d(TAG, "sessionId：" + sessiongId);
+        Log.d(TAG, "偏好设置中的sessionId：" + sp.getString("sessionId", ""));
+        OkHttpUtils.get().url(ConnectUrl.LOGIN_URL)
+                .addParams("userName", name)
+                .addParams("passWord", pwd)
+                .addParams("sessionId", sessiongId)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        ToastUtils.showShortToast("网络故障，请检查网络！");
+                        btnLogin.setEnabled(true);
+                        progressDialog.dismiss();
+                    }
 
+                    @Override
+                    public void onResponse(String response, int id) {
+                        btnLogin.setEnabled(true);
+                        progressDialog.dismiss();
+                        sp.edit().putString("userName",name).apply();
+                        sp.edit().putString("passWord",pwd).apply();
+                        String msg, status;
+                        Log.d(TAG, "登录返回的数据："+response);
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            status = object.getString("status");
+                            msg = object.getString("message");
+                            if ("200".equals(status)) {
+                                ToastUtils.showShortToast(msg);
+                                Intent intent=new Intent(context,MainActivity.class);
+                                startActivity(intent);
+                                isLogin=true;
+                                finish();
+                            }else {
+                                ToastUtils.showShortToast(msg);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
     }
 
+
     //检查输入是否为空
-    private boolean validate(){
+    private boolean validate() {
         boolean valid = true;
 
-        String name = et_name.getText().toString();
-        String pwd = et_pwd.getText().toString();
-        if (TextUtils.isEmpty(name)){
-            et_name.setError("账号不能为空");
+        name = etName.getText().toString();
+        pwd = etPwd.getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            etName.setError("账号不能为空");
             valid = false;
         } else {
-            et_name.setError(null);
+            etName.setError(null);
         }
-        if (TextUtils.isEmpty(pwd)){
-            et_pwd.setError("密码不能为空");
+        if (TextUtils.isEmpty(pwd)) {
+            etPwd.setError("密码不能为空");
             valid = false;
         } else {
-            et_pwd.setError(null);
+            etPwd.setError(null);
         }
 
         return valid;
